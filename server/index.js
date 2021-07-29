5e62f56bf32bbb43c22d60c1ca50a880a0b90f3f
Pratyush Tulsian <pratyushtulsian@gmail.com>
2021-07-28 23:17:23 -0400

    added dotenv

--- server/index.js
+++ server/index.js
@@ -2,23 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import tradeRoutes from "./routes/trades.js";
+import dotenv from "dotenv";
 
 const app = express();
-
-
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/trades", tradeRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://pratty:pratty@cluster0.bplqq.mongodb.net/Tradel?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
